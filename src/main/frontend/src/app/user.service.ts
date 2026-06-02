747a7b4935f3f5bb666044ed0addabcd7aecd5e1
Dominik-3 <dominik.comerzan.024@pslib.cz>
2026-06-02 12:56:26 +0200

    commit30

--- src/main/frontend/src/app/user.service.ts
+++ src/main/frontend/src/app/user.service.ts
@@ -5,7 +5,7 @@ import { User } from './user.model';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-  private apiUrl = 'http://dominik-svc:8080/api/users';
+  private apiUrl = '/api/users';
 
   constructor(private http: HttpClient) {}
 
